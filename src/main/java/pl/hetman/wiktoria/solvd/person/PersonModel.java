1e4fa6745d0a3a0fdb12ce0737169ea1d0fc15d2
wiktoriahet <wiktoriaspyc@gmail.com>
2023-11-01 20:43:44 +0100

    created person interface, refactored code

---
+++ src/main/java/pl/hetman/wiktoria/solvd/person/PersonModel.java
@@ -0,0 +1,11 @@
+package pl.hetman.wiktoria.solvd.person;
+
+public abstract class PersonModel implements Person{
+
+    void printInformation(){}
+
+    @Override
+    public void createAPerson() {
+
+    }
+}
